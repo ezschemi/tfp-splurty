55b1c10dc5abd2c5b303d08043b0d79970cb8cc8
Enrico Zschemisch <enrico@vicampus.com>
2016-10-17 22:02:12 +0200

    - finishing touches

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ApplicationRecord
-  #validates :saying, presence: true, length: { maximum 140, minimum: 3 }
-  #validates :author, presence: true, length: { maximum 50, minimum: 3 }
-  validates :saying, presence: true
-  validates :author, presence: true
+	# length: {} creates a hash map (or array?)
+	# maximum: 140 ==> :maximum is a symbol which is the key, 140 the value
+	# 
+  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :author, presence: true, length: { maximum: 50,  minimum: 3 }
 end
